ee772ef860c92379e6aca25ee4b739fc6d2aaff0
cmiranda1101 <139823343+cmiranda1101@users.noreply.github.com>
2023-07-22 14:55:16 -0400

    Save between sessions
    Added code that allows the user to save and load colors between sessions

--- Assets/Scripts/MenuUIHandler.cs
+++ Assets/Scripts/MenuUIHandler.cs
@@ -12,17 +12,29 @@ public class MenuUIHandler : MonoBehaviour
 {
     public ColorPicker ColorPicker;
 
+    private void Start()
+    {
+        ColorPicker.Init();
+        //this will call the NewColorSelected function when the color picker have a color button clicked.
+        ColorPicker.onColorChanged += NewColorSelected;
+        ColorPicker.SelectColor(MainManager.Instance.TeamColor);
+    }
+
     public void NewColorSelected(Color color)
     {
         // add code here to handle when a color is selected
         MainManager.Instance.TeamColor = color;
     }
     
-    private void Start()
+    public void SaveColorClicked()
     {
-        ColorPicker.Init();
-        //this will call the NewColorSelected function when the color picker have a color button clicked.
-        ColorPicker.onColorChanged += NewColorSelected;
+        MainManager.Instance.SaveColor();
+    }
+
+    public void LoadColorClicked()
+    {
+        MainManager.Instance.LoadColor();
+        ColorPicker.SelectColor(MainManager.Instance.TeamColor);
     }
 
     public void StartNew()
@@ -32,6 +44,7 @@ public class MenuUIHandler : MonoBehaviour
 
     public void Exit()
     {
+        MainManager.Instance.SaveColor();
 #if UNITY_EDITOR 
         EditorApplication.ExitPlaymode();
 #else
